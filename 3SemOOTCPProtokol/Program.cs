38e8856e2b75602ae949b9a21116d04f8db04096
Cyperf <drageon@live.dk>
2024-09-29 16:24:44 +0200

    tcp client side troubles

--- 3SemOOTCPProtokol/Program.cs
+++ 3SemOOTCPProtokol/Program.cs
@@ -1,131 +1,182 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
+using System.Reflection.Emit;
+using System.Reflection.PortableExecutable;
+using System.Text;
 using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+void ChooseUser()
+{
+    Console.WriteLine("Server or User?");
+    string who = Console.ReadLine();
 
-
-void ChooseUser() { 
-Console.WriteLine("Server or User?");
-string who = Console.ReadLine();
-
-if (who.ToLower() == "server") 
-{ 
-
-Console.WriteLine("TCP Server:");
-
-TcpListener listener = new TcpListener(IPAddress.Any, 21);
-
-listener.Start();
-
-while (true) { 
-TcpClient socket = listener.AcceptTcpClient();
-Task.Run(()=>HandleClient(socket));
-}
-
-void HandleClient(TcpClient socket)
+    if (who.ToLower() == "server")
     {
-        NetworkStream ns = socket.GetStream();
+        Console.WriteLine("TCP Server:");
 
-        StreamReader reader = new StreamReader(ns);
-        StreamWriter writer = new StreamWriter(ns);
+        TcpListener listener = new TcpListener(IPAddress.Any, 21);
 
-        bool keepListening = true;
+        listener.Start();
 
-        while (keepListening)
+        while (true)
         {
-            string message = reader.ReadLine();
-
-            Console.WriteLine("Client send: " + message);
-
-            writer.WriteLine(message);
-            writer.Flush();
-
-            switch (message.ToLower())
-            {
-                case "stop":
-                    writer.WriteLine("Goodbye");
-                    writer.Flush();
-                    keepListening = false;
-                    break;
-
-                case "random":
-                    writer.WriteLine(HandleNumbers("random"));
-                    writer.Flush();
-                    break;
-
-                case "add":
-                    writer.WriteLine(HandleNumbers("add"));
-                    writer.Flush();
-                    break;
-
-                case "subtract":
-                    writer.WriteLine(HandleNumbers("subtract"));
-                    writer.Flush();
-                    break;
-
-                default:
-                    writer.WriteLine("Invalid command. The available commands are: Add, Subtract, Random or Stop");
-                    writer.Flush();
-                    break;
-            }
+            TcpClient socket = listener.AcceptTcpClient();
+            Task.Run(() => HandleClient(socket));
         }
 
-        int HandleNumbers(string type)
+        void HandleClient(TcpClient socket)
         {
-            int number1 = 0;
-            int number2 = 0;
+            NetworkStream ns = socket.GetStream();
 
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
 
-            Random randomNumber = new Random();
+            bool keepListening = true;
 
-            writer.WriteLine("Input numbers");
-            writer.Flush();
-            string numbers = reader.ReadLine();
-            var splitNumbers = numbers.Split(' ');
-
-            if (splitNumbers.Length == 2)
+            while (keepListening)
             {
-                int.TryParse(splitNumbers[0], out number1);
-                int.TryParse(splitNumbers[1], out number2);
+                string message = reader.ReadLine();
+
+                Console.WriteLine("Client send: " + message);
+
+                switch (message.ToLower())
+                {
+                    case "user connected":
+                        Console.WriteLine("Server just sent welcome message");
+                        writer.WriteLine("Welcome. The available commands are: Add, Subtract, Random or Stop");
+                        writer.Flush();
+                        break;
+
+                    case "stop":
+                        writer.WriteLine("Goodbye");
+                        writer.Flush();
+                        keepListening = false;
+                        break;
+
+                    case "random":
+                        writer.WriteLine(HandleNumbers("random"));
+                        writer.Flush();
+                        break;
+
+                    case "add":
+                        writer.WriteLine(HandleNumbers("add"));
+                        writer.Flush();
+                        break;
+
+                    case "subtract":
+                        writer.WriteLine(HandleNumbers("subtract"));
+                        writer.Flush();
+                        break;
+
+                    default:
+                        writer.WriteLine("Invalid command. The available commands are: Add, Subtract, Random or Stop");
+                        writer.Flush();
+                        break;
+                }
             }
-            else
+
+            int HandleNumbers(string type)
             {
-                writer.WriteLine("Invalid input");
                 writer.Flush();
-            }
+                int number1 = 0;
+                int number2 = 0;
 
-            switch (type)
-            {
 
-                case "random":
-                        if (number1 < number2) { 
-                    return randomNumber.Next(number1, number2) + 1;
+                Random randomNumber = new Random();
+
+                writer.WriteLine("Input numbers");
+                writer.Flush();
+
+                string numbers = reader.ReadLine();
+                var splitNumbers = numbers.Split(' ');
+
+                if (splitNumbers.Length == 2)
+                {
+                    int.TryParse(splitNumbers[0], out number1);
+                    int.TryParse(splitNumbers[1], out number2);
+                }
+                else
+                {
+                    writer.WriteLine("Invalid input");
+                    writer.Flush();
+                }
+
+                switch (type)
+                {
+
+                    case "random":
+                        if (number1 < number2)
+                        {
+                            return randomNumber.Next(number1, number2) + 1;
                         }
                         return randomNumber.Next(number2, number1) + 1;
 
                     case "add":
 
-                    return number1 + number2;
+                        return number1 + number2;
 
-                case "subtract":
+                    case "subtract":
 
-                    return number1 - number2;
+                        return number1 - number2;
 
+                }
+                return 0;
             }
-            return 0;
         }
-    }
-    
-} 
-else if (who.ToLower() == "user")
-{
 
-} else
-{
-    Console.WriteLine("Invalid input.");
+    }
+    else if (who.ToLower() == "user")
+    {
+        TcpClient client = new TcpClient();
+        IPEndPoint clientEndpoint = null;
+        
+        bool connected = false;
+        
+        client.Connect("127.0.0.1", 21);
+        NetworkStream stream = client.GetStream();
+        NetworkStream ns = client.GetStream();
+        //The StreamReader is an easier way to read data from a Stream, it uses the NetworkStream
+        StreamReader reader = new StreamReader(ns);
+        //The StreamWriter is an easier way to write data to a Stream, it uses the NetworkStream
+        StreamWriter writer = new StreamWriter(ns);
+        writer.WriteLine("User connected");
+        writer.Flush();
+        string response = reader.ReadLine();
+        Console.WriteLine(response);
+
+        connected = true;
+        
+        while (connected) 
+        {
+            string message = Console.ReadLine();
+
+            writer.WriteLine(message);
+
+            writer.Flush();
+
+            Console.WriteLine(response);
+
+            if (message.ToLower() == "stop")
+            {
+                writer.WriteLine("User disconnected");
+                writer.Flush();
+                connected = false;
+            }
+
+            client.Close();
+        }
+
+
+    }
+    else
+    {
+        Console.WriteLine("Invalid input.");
         ChooseUser();
-}
+    }
+
 }
 
 ChooseUser();
