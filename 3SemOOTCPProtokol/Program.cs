31ed2e11b68cd128f34549d30a9efa0f3cd29b56
Cyperf <drageon@live.dk>
2024-09-29 21:52:02 +0200

    godnat

--- 3SemOOTCPProtokol/Program.cs
+++ 3SemOOTCPProtokol/Program.cs
@@ -8,9 +8,10 @@ using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Text.RegularExpressions;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text.Json;
 void ChooseUser()
 {
-    Console.WriteLine("Server or User?");
+    Console.WriteLine("Server, JSON Server or User?");
     string who = Console.ReadLine();
 
     if (who.ToLower() == "server")
@@ -122,7 +123,115 @@ void ChooseUser()
         }
 
     }
+    else if((who.ToLower() == "json server"))
+    {
+        Console.WriteLine("TCP JSON Server:");
 
+        TcpListener listener = new TcpListener(IPAddress.Any, 22);
+
+        listener.Start();
+
+        while (true)
+        {
+            TcpClient socket = listener.AcceptTcpClient();
+            Task.Run(() => HandleClient(socket));
+        }
+
+        void HandleClient(TcpClient socket)
+        {
+            NetworkStream ns = socket.GetStream();
+
+            StreamReader reader = new StreamReader(ns);
+            StreamWriter writer = new StreamWriter(ns);
+
+            bool keepListening = true;
+
+            while (keepListening)
+            { 
+                string? message = reader.ReadLine();
+                
+
+                Console.WriteLine(JsonSerializer.Serialize(message));
+
+                switch (message.ToLower())
+                {
+                    case "user connected":
+                        writer.WriteLine("Welcome. The available commands are: Add, Subtract, Random or Stop");
+                        writer.Flush();
+                        break;
+
+                    case "stop":
+                        writer.WriteLine("Goodbye");
+                        writer.Flush();
+                        keepListening = false;
+                        break;
+
+                    case "random":
+                        writer.WriteLine(HandleNumbers("random"));
+                        writer.Flush();
+                        break;
+
+                    case "add":
+                        writer.WriteLine(HandleNumbers("add"));
+                        writer.Flush();
+                        break;
+
+                    case "subtract":
+                        writer.WriteLine(HandleNumbers("subtract"));
+                        writer.Flush();
+                        break;
+
+                    default:
+                        writer.WriteLine("Invalid command. The available commands are: Add, Subtract, Random or Stop");
+                        writer.Flush();
+                        break;
+                }
+            }
+
+            int HandleNumbers(string type)
+            {
+                int number1 = 0;
+                int number2 = 0;
+
+                Random randomNumber = new Random();
+
+                writer.WriteLine("Input numbers");
+                writer.Flush();
+
+                string numbers = reader.ReadLine();
+                var splitNumbers = numbers.Split(' ');
+
+                if (splitNumbers.Length == 2)
+                {
+                    int.TryParse(splitNumbers[0], out number1);
+                    int.TryParse(splitNumbers[1], out number2);
+                    switch (type)
+                    {
+                        case "random":
+                            if (number1 < number2)
+                            {
+                                return randomNumber.Next(number1, number2) + 1;
+                            }
+                            return randomNumber.Next(number2, number1) + 1;
+
+                        case "add":
+
+                            return number1 + number2;
+
+                        case "subtract":
+
+                            return number1 - number2;
+
+                    }
+                }
+                else
+                {
+                    return 0;
+                }
+                return 0;
+            }
+        }
+    }
     else if (who.ToLower() == "user")
     {
         TcpClient client = new TcpClient();
